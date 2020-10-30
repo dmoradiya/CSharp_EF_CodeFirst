f4c8acc6706f9e728f74c9f53ffe8e9164482454
dhmec <dharmoradiya@gmail.com>
2020-10-30 10:26:22 -0600

    Add Initial Migration

--- CSharp_CodeFirst/Model/Vehicle.cs
+++ CSharp_CodeFirst/Model/Vehicle.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace CSharp_CodeFirst.Model
 {
+    [Table("vehicle")]
     class Vehicle
     {
+        [Key]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        
+        [Column(TypeName = "varchar(30)")]
+        public int Manufacturer { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string Model { get; set; }
+        [Column(TypeName = "int(10)")]
+        public int ModelYear { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(30)")]
+        public string Colour { get; set; }
+
+        
     }
 }
